a11d1c1e9a27a2e1dda3d67f982b5eeedf6d3300
eminmuhemmedi <muemin17631@sabah.edu.az>
2019-01-17 20:36:08 +0400

    noAPI

--- 0request.php
+++ 0request.php
@@ -4,9 +4,9 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, ,DELETE , PUT , OPTIONS");
 
 
-include_once 'controller.class.php' ;
-include_once 'core.class.php' ;
-include_once 'view.class.php' ;
+include_once 'src/controller.class.php' ;
+include_once 'src/core.class.php' ;
+include_once 'src/view.class.php' ;
 
 $data = [
 
